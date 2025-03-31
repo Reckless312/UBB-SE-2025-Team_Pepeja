9f6feca55adc73f147a56c317d258dfaa3fb4351
Justin <chinweikejustin@gmail.com>
2025-03-31 13:19:23 +0300

    Everything implemented except the expanded review

---
+++ Reviews/SteamCommunity.Reviews/SteamCommunity.Reviews/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace SteamCommunity.Reviews.Models
+{
+    public class User
+    {
+        public int UserIdentifier { get; set; }
+        public string UserName { get; set; } = string.Empty;
+
+        public string ProfilePictureUrlOrPath { get; set; } = string.Empty;
+
+        // Optional methods to retrieve User information
+        public int getUserId() => UserIdentifier;
+        public string getName() => UserName;
+
+        public string getProfilePicture() => ProfilePictureUrlOrPath;
+    }
+}
